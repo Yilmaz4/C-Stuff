d5c74d9169eaaf8ce6f973fa3e6e96f44d16d95f
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2022-08-03 03:16:44 +0300

    Add circular array

--- src/circulararray.h
+++ src/circulararray.h
@@ -1,13 +1,38 @@
 #pragma once
 
-template <typename type> class CircularArray {
-	type base_addr[];
-	size_t size = 0;
-public:
-	CircularArray(size_t size) : base_addr[size] {
+#include <iostream>
 
+typedef unsigned long long index_t;
+
+template <typename type> class CircularArray {
+	type* base_addr;
+	size_t length = 0;
+public:
+	explicit CircularArray(const size_t size) : length(size) {
+		base_addr = new type[size];
 	}
-	type operator [] (int index) {
-		return *(base_addr + (index % size))
+	CircularArray(std::initializer_list<type> const& list) {
+		length = list.size();
+		base_addr = new type[length];
+		for (index_t i = 0; i < length; i++)
+			(*this)[i] = *((type*)list.begin() + i);
+	}
+	inline void print() const {
+		std::cout << "[";
+		for (index_t i = 0; i < length; i++)
+			std::cout << (*this)[i] << ((i != length - 1) ? ", " : "");
+		std::cout << "]";
+	}
+	inline size_t size() const noexcept {
+		return length;
+	}
+	inline bool empty() const noexcept {
+		return !length;
+	}
+	type& operator [] (int index) const noexcept {
+		return *(base_addr + (index % (int)length));
+	}
+	operator int * __ptr64() const noexcept {
+		return base_addr;
 	}
 };
