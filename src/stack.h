a46d77c9a53c32ccdbdc06ca16eaaf6663f86f8e
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2022-08-05 02:01:41 +0300

    Replace void* with Node*, add initializer_list support for constructor

--- src/stack.h
+++ src/stack.h
@@ -1,47 +1,54 @@
 #pragma once
 
-template <typename type> class Stack {
-	void* top_ptr = nullptr;
-	size_t length = 0;
-protected:
+#include <iostream>
+#include <exception>
+
+template <typename type> class LinkedStack {
 	struct Node {
 		Node(type data, Node* ptr) {
-			this->data = (type*)malloc(sizeof(data));
 			*this->data = data;
 			this->ptr = ptr;
 		}
 		~Node() {
 			free((void*)this->data);
 		}
-		type* data;
+		type* data = (type*)malloc(sizeof(type));
 		Node* ptr;
 	};
+
+	Node* top_ptr = nullptr;
+	size_t length = 0;
 public:
 	struct stack_empty_error : public std::exception {
 		virtual const char* what() const throw() {
 			return "Stack is empty";
 		}
 	};
-	Stack(void) = default;
+	LinkedStack(void) = default;
+	LinkedStack(std::initializer_list<type> const& list) {
+		for (auto const& obj : list) {
+			push(obj);
+		}
+	}
 
 	inline void push(type obj) {
-		top_ptr = (void*)new Node(obj, (Node*)top_ptr);
+		top_ptr = new Node(obj, top_ptr);
 		length++;
 	}
 	inline type pop() {
 		if (top_ptr == nullptr)
 			throw stack_empty_error();
-		type data = *((Node*)top_ptr)->data;
-		Node* addr = ((Node*)top_ptr)->ptr;
+		type data = *top_ptr->data;
+		Node* addr = top_ptr->ptr;
 		delete top_ptr;
-		top_ptr = (void*)addr;
+		top_ptr = addr;
 		length--;
 		return data;
 	}
 	inline type top() {
 		if (top_ptr == nullptr)
 			throw stack_empty_error();
-		return *((Node*)top_ptr)->data;
+		return *top_ptr->data;
 	}
 	inline size_t size() const {
 		return length;
@@ -49,11 +56,11 @@ public:
 	inline bool empty() const {
 		return !this->length;
 	}
-	friend auto operator << (std::ostream& os, Stack const& obj) -> std::ostream& {
+	friend auto operator << (std::ostream& os, LinkedStack const& obj) -> std::ostream& {
 		std::cout << "[";
 		index_t i = 0;
-		for (void* addr = obj.top_ptr; addr; addr = ((Node*)addr)->ptr, i++)
-			std::cout << ((Node*)addr)->data << ((i == obj.get_length() - 1) ? "" : ", ");
+		for (Node* addr = obj.top_ptr; addr; addr = addr->ptr, i++)
+			std::cout << addr->data << ((i == obj.get_length() - 1) ? "" : ", ");
 		std::cout << "]";
 		return os;
 	}
