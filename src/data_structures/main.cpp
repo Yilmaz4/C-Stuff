b9c5c915cdc198241885d63352e2efcbd3aef874
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2022-08-11 23:03:50 +0300

    Test for LinkedString

--- src/data_structures/main.cpp
+++ src/data_structures/main.cpp
@@ -5,7 +5,15 @@
 #include "queue.h"
 #include "stack.h"
 #include "tree.h"
+#include "string.h"
+
+using namespace tree;
+using namespace string;
 
 int main(int argc, char* argv[]) {
-	tree::BinarySearchTree<int> myTree;
+	LinkedString str1 = "Hello World!";
+	LinkedString str2 = "Hello Neighbor!";
+
+	str1.swap(str2);
+	std::cout << str1 << std::endl << str2;
 }
