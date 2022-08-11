1387f3cdbef03e5867ac75bd0e9f6e9311bbce4f
Yılmaz Alpaslan <ymzymz2007@gmail.com>
2022-08-11 23:02:12 +0300

    Add Graph (unimplemented)

---
+++ src/data_structures/graph.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <cmath>
+
+namespace graph {
+	typedef size_t index_t;
+
+	template <typename type> class Graph {
+		struct Node {
+
+		};
+	};
+}
